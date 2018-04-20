85d2fd56b2c900115ff98f704b09b8e5443e8eba
gongruihua <1035279462@qq.com>
2018-04-20 10:46:29 +0800

    111

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(121212);
+        System.out.println(1212122222);
         System.out.println(new App().getMessage());
     }
 
